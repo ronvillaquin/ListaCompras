acb85d854155e74293b6ad4369d2ca7ae61126d8
ronal <ronaldr.vq@gmail.com>
2021-06-09 17:06:00 -0500

    creando los fragments check y no check

---
+++ app/src/main/java/com/rrvq/listacompras/productos/CheckFragment.java
@@ -0,0 +1,26 @@
+package com.rrvq.listacompras.productos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rrvq.listacompras.R;
+
+public class CheckFragment extends Fragment {
+
+
+    public CheckFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_check, container, false);
+    }
+}
