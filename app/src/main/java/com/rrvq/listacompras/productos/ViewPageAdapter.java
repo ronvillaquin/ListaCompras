4e7bd837ed4ee0dc22eab3e177dfa773b0924f24
ronal <ronaldr.vq@gmail.com>
2021-06-09 23:56:00 -0500

    creando viewPager adapter con array list  para la cantidad de tabs y de fragments

---
+++ app/src/main/java/com/rrvq/listacompras/productos/ViewPageAdapter.java
@@ -0,0 +1,34 @@
+package com.rrvq.listacompras.productos;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewPageAdapter extends FragmentStateAdapter {
+
+    ArrayList<Fragment> arrayList = new ArrayList<>();
+
+    public ViewPageAdapter(@NonNull FragmentManager fragmentManager, @NonNull  Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        arrayList.add(fragment);
+    }
+
+}
